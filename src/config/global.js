e324056c9125b99ddb139316a0f0a585601ab8a6
Oscar Espitia <odespitia@sena.edu.co>
2024-09-18 05:01:24 -0500

    ajustes cfa

--- src/config/global.js
+++ src/config/global.js
@@ -152,7 +152,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CFA_02_13330035_DU.zip',
       },
       {
         icono: 'fas fa-download',
